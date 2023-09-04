edc673accd5b5c2c6dd48b5be7bad9268788a399
Abdulrahman ayman adi <142898159+Abdulrahman-the-wanderer@users.noreply.github.com>
2023-09-04 16:09:35 +0300

    work

---
+++ main.py
@@ -0,0 +1,19 @@
+def my_name(xeo):
+    print(f"my name is {xeo}")
+name = "abdulrahman"
+my_name(name)
+def my_meal(food,drink):
+    print(f"I like to eat {food} and while drinking {drink}")
+f = "egg"
+d = "orang juce"
+my_meal(f,d)
+def cube(number):
+    return print(number * number * number)
+def by_three(number):
+    if  number % 3 == 0 : 
+        cube(number)
+    else :
+        print('false')
+    
+
+by_three(3)
